4a64714149610fee1433f01304c2b8b84980e80a
Juan Mateo Frasinelli <d46710280@alumnos.itr.edu.ar>
2024-06-03 11:21:17 -0300

    Se añadieron fuentes y se mejoro el css

--- src/Grupo_2/Grupo2.jsx
+++ src/Grupo_2/Grupo2.jsx
@@ -4,13 +4,13 @@ import './Grupo2.css';
 function Introduccion() {
   return (
     <div>
-      <h1>Trastorno por Deficit de Atencion e Hiperactividad:</h1>
-      <p>El Trastorno por Déficit de Atención e Hiperactividad (TDAH) se trata de un desorden neuropsiquiátrico del desarrollo<br />
+      <h1 class = "title"><font color="#00107a">T</font>rastorno por<br/> <font color="#00107a">D</font>eficit de <br/><font color="#00107a">A</font>tencion e <br/><font color="#00107a">H</font>iperactividad:</h1>
+      <p>El Trastorno por Déficit de Atención e Hiperactividad (TDAH) se trata de un desorden neuropsiquiátrico del desarrollo
         que afecta la capacidad para prestar atención, controlar ciertos impulsos y/o actividad de los que lo padecen.</p>
-      <p>Es encontrado más común en jóvenes menores de 16 o 17 años, pero estos también pueden persistir en la edad adulta.<br />
+      <p>Es encontrado más común en jóvenes menores de 16 o 17 años, pero estos también pueden persistir en la edad adulta.
 
       </p>
-      <p>El diagnóstico de TDAH se establece clínicamente mediante una evaluación exhaustiva, como el DSM-5, que considera los<br />
+      <p>El diagnóstico de TDAH se establece clínicamente mediante una evaluación exhaustiva, como el DSM-5, que considera los
         aspectos médicos, del desarrollo, educativos, del contexto y psicológicos de cada individuo.</p>
       <p>Los síntomas del TDAH pueden variar en intensidad y presentación.</p>
       <p>Existen 3 tipos:</p>
@@ -25,7 +25,7 @@ function Introduccion() {
 
 function SintomasYTratamientos() {
   return (
-    <>
+    <div>
       <p>Hay síntomas que ayudan a reconocer el TDAH, estos son:</p>
       <ul>
         <li>
@@ -40,18 +40,22 @@ function SintomasYTratamientos() {
       <p>Los tratamientos para el TDAH son:</p>
       <ul>
         <li>
-          <p>- Medicamentos estimulantes (como metilfenidato o dextroanfetamina).</p>
-          <p>- Intervención conductual.</p>
-          <p>- Desarrollo de buenos hábitos para facilitar el día a día.</p>
+          <p>Medicamentos estimulantes (como metilfenidato o dextroanfetamina).</p>
+        </li>
+        <li>
+          <p>Intervención conductual.</p>
+        </li>
+        <li>
+          <p>Desarrollo de buenos hábitos para facilitar el día a día.</p>
         </li>
       </ul>
-    </>
+    </div>
   );
 }
 
 function CausasYManejo() {
   return (
-    <>
+    <div>
       <p>Las causas para el TDAH no son específicas. Algunas razones explícitas para el trastorno pueden ser:</p>
       <ul>
         <li>
@@ -66,12 +70,16 @@ function CausasYManejo() {
       <p>Los tratamientos para el TDAH son:</p>
       <ul>
         <li>
-          <p>- Medicamentos estimulantes (como metilfenidato o dextroanfetamina).</p>
-          <p>- Intervención conductual.</p>
-          <p>- Desarrollo de buenos hábitos para facilitar el día a día.</p>
+          <p>Medicamentos estimulantes (como metilfenidato o dextroanfetamina).</p>
+        </li>
+        <li>
+          <p>Intervención conductual.</p>
+        </li>
+        <li>
+          <p>Desarrollo de buenos hábitos para facilitar el día a día.</p>
         </li>
       </ul>
-    </>
+    </div>
   );
 }
 
@@ -91,8 +99,11 @@ function Grupo2() {
     <div id="content">
       <h1>Página destinada para el contenido del grupo 2</h1>
       <Introduccion />
+      <br />
       <SintomasYTratamientos />
+      <br />
       <CausasYManejo/>
+      <br />
       <Conclusion />
     </div>
   )
