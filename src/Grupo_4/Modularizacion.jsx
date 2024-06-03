e4a18d0bb0b10148444d28fa6730a22acbbf0816
Joaquin Ignacio Fritz Carrizo <d48125437@alumnos.itr.edu.ar>
2024-06-03 11:29:11 -0300

    Modularizacion

---
+++ src/Grupo_4/Modularizacion.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Grupo4.css';
+
+function Modularizacion() {
+  return (
+    <a href="/imagenes">
+        <img src="downloads.png" alt="foto pertinente al tema" />
+      </a>
+  );
+}
+
+export default Modularizacion;
