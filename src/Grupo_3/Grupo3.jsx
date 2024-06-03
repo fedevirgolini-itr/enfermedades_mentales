f851b855f5f01e64fb208d962f7d3084198e0a77
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-06-03 16:59:06 -0300

    v5

--- src/Grupo_3/Grupo3.jsx
+++ src/Grupo_3/Grupo3.jsx
@@ -24,29 +24,28 @@ function Grupo3() {
       <label class="form-check-label Position" for="flexSwitchCheckDefault"></label>
     </div>
       <h1 id='titulo' class={modo}>Ansiedad</h1>
-    <div class='text' >
-      <p class={modo} >
+    <div class={modo} >
+      <p>
         La ansiedad es un trastorno común que afecta a muchas personas en todo el mundo, manifestándose a través de síntomas físicos y emocionales. Es crucial entender sus síntomas, causas y opciones de tratamiento para brindar un apoyo efectivo a quienes la padecen.
       </p>
-      <h4 id='subtitulo' class={modo}>¿Cómo definimos la ansiedad en sí?</h4>
-      <p class={modo}>
+      <h4 id='subtitulo'>¿Cómo definimos la ansiedad en sí?</h4>
+      <p>
         La ansiedad es un sentimiento de miedo, temor e inquietud. Puede hacer que sude, se sienta inquieto y tenso, y tener palpitaciones acelerdas.
       </p>
-
       <h3>Una de las formas de detectarla es a través de los síntomas. Estos síntomas son:</h3>
       <ul id='lista'>
-        <li>•Dificultad para respirar</li>
-        <li>•Palpitaciones aceleradas</li>
-        <li>•Sensación de mareo</li>
-        <li>•Sudor</li>
-        <li>•La presentación de varios eventos de miedo</li>
-        <li>•Temblores</li>
-        <li>•Inquietud</li>
-        <li>•Molestias inexplicables</li>
-        <li>•Dolores</li>
-        <li>•Pánico</li>
-        <li>•Debilidad o cansancio</li>
-        <li>•Problemas para concentrarse o para pensar en algo que no sea la preocupación actual</li>
+        <li>Dificultad para respirar</li>
+        <li>Palpitaciones aceleradas</li>
+        <li>Sensación de mareo</li>
+        <li>Sudor</li>
+        <li>La presentación de varios eventos de miedo</li>
+        <li>Temblores</li>
+        <li>Inquietud</li>
+        <li>Molestias inexplicables</li>
+        <li>Dolores</li>
+        <li>Pánico</li>
+        <li>Debilidad o cansancio</li>
+        <li>Problemas para concentrarse o para pensar en algo que no sea la preocupación actual</li>
       </ul>
     </div>
     <Ventana mod={modo}/>
