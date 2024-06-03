75b78df2db51c49c9f57db895f43f121d1fcad41
Juan Mateo Frasinelli <d46710280@alumnos.itr.edu.ar>
2024-06-03 16:36:17 -0300

    Se añadieron imagenes

--- src/Grupo_2/Grupo2.jsx
+++ src/Grupo_2/Grupo2.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Grupo2.css';
+import FOTO1 from "./TDHA1.jpg"
+import FOTO2 from "./TDAH2.jpg"
+import FOTO3 from "./TDAH3.jpeg"
 
 function Introduccion() {
   return (
     <div>
-      <h1 class = "title"><font color="#00107a">T</font>rastorno por<br/> <font color="#00107a">D</font>eficit de <br/><font color="#00107a">A</font>tencion e <br/><font color="#00107a">H</font>iperactividad:</h1>
+      <h1 class = "title" id='mainTitle'><font color="#00107a">T</font>rastorno por<br/> <font color="#00107a">D</font>eficit de <br/><font color="#00107a">A</font>tencion e <br/><font color="#00107a">H</font>iperactividad:</h1>
       <p>El Trastorno por Déficit de Atención e Hiperactividad (TDAH) se trata de un desorden neuropsiquiátrico del desarrollo
         que afecta la capacidad para prestar atención, controlar ciertos impulsos y/o actividad de los que lo padecen.</p>
       <p>Es encontrado más común en jóvenes menores de 16 o 17 años, pero estos también pueden persistir en la edad adulta.
@@ -26,6 +29,7 @@ function Introduccion() {
 function SintomasYTratamientos() {
   return (
     <div>
+      <img src={FOTO1} alt="" width="60%"/>
       <p>Hay síntomas que ayudan a reconocer el TDAH, estos son:</p>
       <ul>
         <li>
@@ -56,29 +60,19 @@ function SintomasYTratamientos() {
 function CausasYManejo() {
   return (
     <div>
+      <img src={FOTO3} alt="" width="55%"/>
       <p>Las causas para el TDAH no son específicas. Algunas razones explícitas para el trastorno pueden ser:</p>
       <ul>
-        <li>
-          <p>
-            <strong>Problemas de atención:</strong> Falta de atención involuntaria a detalles; parece no escuchar, incluso cuando se le habla directamente; falta de concentración o distracción sencilla; olvido de cosas diarias o pérdida de objetos; descuidarse y cometer errores sencillos; problemas de organización; reuso de hacer ejercicios que requieran de gran esfuerzo mental; etc.
-          </p>
-          <p>
-            <strong>Hiperactividad e impulsividad:</strong> Cambios de humor; impaciencia; hablar mucho; problemas para respetar turnos; problemas para relacionarse; moverse nerviosamente; estar en movimiento ayuda pensar; etc.
-          </p>
-        </li>
+      <li><p>Factores genéticos, bioquímicos, sensitivomotores, fisiológicos y/o conductuales.</p></li>
+        <li><p>Exposición al plomo.</p></li>
+        <li><p>Deficiencia de hierro.</p></li>
+        <li><p>Peso al nacer menor a  1.500 g.</p></li>
+        <li><p>Daño neurológico.</p></li>
+        <li><p>Apnea obstructiva del sueño.</p></li>
+        <li><p>Exposición prenatal a alcohol, tabaco y/o cocaína.</p></li>
+        <li><p>Traumatismo craneoencefálico.</p></li>
       </ul>
-      <p>Los tratamientos para el TDAH son:</p>
-      <ul>
-        <li>
-          <p>Medicamentos estimulantes (como metilfenidato o dextroanfetamina).</p>
-        </li>
-        <li>
-          <p>Intervención conductual.</p>
-        </li>
-        <li>
-          <p>Desarrollo de buenos hábitos para facilitar el día a día.</p>
-        </li>
-      </ul>
+      <p>Además del tratamiento,en el manejo del TDAH en la vida cotidiana se necesita un control de las actividades, además de crear un ambiente organizado y aprender estrategias de manejo de tiempo y tareas. El apoyo emocional y social también es fundamental para ayudar a las personas con TDAH a enfrentar los desafíos asociados con el trastorno y mejorar su calidad de vida, también acudir a profesionales ayuda al autocontrol y manejo del trastorno mediante técnicas.</p>
     </div>
   );
 }
@@ -86,7 +80,8 @@ function CausasYManejo() {
 function Conclusion() {
   return (
     <div>
-      <h1>Conclusión:</h1>
+      <img src={FOTO2} alt="" />
+      <h1 class = "title">Conclusión:</h1>
       <p>El TDAH es un trastorno que implica falta de atención, hiperactividad/impulsividad, o una combinación, presente más
         común en jóvenes, pero que se pueden presentar en la adultez. Su origen puede ser desconocido pero puede ser tratado
         con Farmacoterapia, terapia o con buenos hábitos.</p>
@@ -97,7 +92,7 @@ function Conclusion() {
 function Grupo2() {
   return (
     <div id="content">
-      <h1>Página destinada para el contenido del grupo 2</h1>
+      <h1 class = "title">Grupo 2</h1>
       <Introduccion />
       <br />
       <SintomasYTratamientos />
