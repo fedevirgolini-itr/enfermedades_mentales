f5944fd726c7648db2f16838d51035dadf2835de
nicolozzZ <135234760+nicolozzZ@users.noreply.github.com>
2024-06-03 16:19:51 -0300

    se actualizo los textos de la funcion CausasYManejo

--- src/Grupo_2/Grupo2.jsx
+++ src/Grupo_2/Grupo2.jsx
@@ -58,30 +58,20 @@ function CausasYManejo() {
     <div>
       <p>Las causas para el TDAH no son específicas. Algunas razones explícitas para el trastorno pueden ser:</p>
       <ul>
-        <li>
-          <p>
-            <strong>Problemas de atención:</strong> Falta de atención involuntaria a detalles; parece no escuchar, incluso cuando se le habla directamente; falta de concentración o distracción sencilla; olvido de cosas diarias o pérdida de objetos; descuidarse y cometer errores sencillos; problemas de organización; reuso de hacer ejercicios que requieran de gran esfuerzo mental; etc.
-          </p>
-          <p>
-            <strong>Hiperactividad e impulsividad:</strong> Cambios de humor; impaciencia; hablar mucho; problemas para respetar turnos; problemas para relacionarse; moverse nerviosamente; estar en movimiento ayuda pensar; etc.
-          </p>
-        </li>
+        <li><p>Factores genéticos, bioquímicos, sensitivomotores, fisiológicos y/o conductuales.</p></li>
+        <li><p>Exposición al plomo.</p></li>
+        <li><p>Deficiencia de hierro.</p></li>
+        <li><p>Peso al nacer menor a  1.500 g.</p></li>
+        <li><p>Daño neurológico.</p></li>
+        <li><p>Apnea obstructiva del sueño.</p></li>
+        <li><p>Exposición prenatal a alcohol, tabaco y/o cocaína.</p></li>
+        <li><p>Traumatismo craneoencefálico.</p></li>
       </ul>
-      <p>Los tratamientos para el TDAH son:</p>
-      <ul>
-        <li>
-          <p>Medicamentos estimulantes (como metilfenidato o dextroanfetamina).</p>
-        </li>
-        <li>
-          <p>Intervención conductual.</p>
-        </li>
-        <li>
-          <p>Desarrollo de buenos hábitos para facilitar el día a día.</p>
-        </li>
-      </ul>
+      <p>Además del tratamiento,en el manejo del TDAH en la vida cotidiana se necesita un control de las actividades, además de crear un ambiente organizado y aprender estrategias de manejo de tiempo y tareas. El apoyo emocional y social también es fundamental para ayudar a las personas con TDAH a enfrentar los desafíos asociados con el trastorno y mejorar su calidad de vida, también acudir a profesionales ayuda al autocontrol y manejo del trastorno mediante técnicas.</p>
     </div>
   );
 }
+
 
 function Conclusion() {
   return (
