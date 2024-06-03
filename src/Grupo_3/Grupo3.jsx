6b7751bab883d4c7cfab9780fcfc691525082737
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-06-03 10:31:13 -0300

    v 3 base lista para empezar a trabajar

--- src/Grupo_3/Grupo3.jsx
+++ src/Grupo_3/Grupo3.jsx
@@ -7,29 +7,28 @@ function Grupo3() {
 
   const ChangeMode=()=>{
     if (modo==="default"){
-      setModo("Ansiedad")
-
-      
-
+      setModo("ansiedad")
     }else{
       setModo("default")
     }
     
   }
   return (
-     <div>
+    <div>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
       <div class="form-check form-switch">
-  <input class="form-check-input Position" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={()=>ChangeMode()}>
-    
-  </input>
-  <label class="form-check-label Position" for="flexSwitchCheckDefault"></label>
-</div>
-      <h1 class={modo}>Página destinada para el contenido del grupo</h1>
-    <Ventana modo={modo}/>
-    <Ventana modo={modo}/>
-    <Ventana modo={modo}/>
+      <input class="form-check-input Position" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={()=>ChangeMode()}>
+        
+      </input>
+      <label class="form-check-label Position" for="flexSwitchCheckDefault"></label>
     </div>
+      <h1 id='titulo' class={modo}>Ansiedad</h1>
+    <div class='text' >
+      <p class={modo} >
+        La ansiedad es un trastorno común que afecta a muchas personas en todo el mundo, manifestándose a través de síntomas físicos y emocionales. Es crucial entender sus síntomas, causas y opciones de tratamiento para brindar un apoyo efectivo a quienes la padecen.
+      </p>
+    </div>
+  </div>
   )
 }
 
