6a7f53e84329050fc3fcc16988f4b13204faa837
Gorra.js <gorrahabla1234@gmail.com>
2024-06-03 19:20:48 -0300

    v1

--- src/Grupo_1/Grupo1.jsx
+++ src/Grupo_1/Grupo1.jsx
@@ -1,11 +1,112 @@
-import React from 'react'
+import React from "react";
+import "./css/index2.css";
 
 function Grupo1() {
   return (
-    <div>
-      <h1>Página destinada para el contenido del grupo</h1>
+    <div class="depresion">
+      <link rel="stylesheet" type="text/css" href="css/index.css"></link>
+      <div class="texto">
+        <center>
+          <h1 class="white">La depresión</h1>
+        </center>
+        <h2 class="white">
+          <u>¿Qué es la depresión?</u>
+        </h2>
+        <p class="whitep">
+          La depresión es un trastorno mental que afecta el estado de ánimo, los
+          pensamientos y el comportamiento de una persona.
+        </p>
+        <h2 class="white">
+          <u>¿Cuáles son sus síntomas?</u>
+        </h2>
+        <p class="whitep">
+          Se caracteriza por sentimientos persistentes de tristeza,
+          desesperanza, desinterés en actividades que solían ser placenteras,
+          falta de energía, dificultades para concentrarse, cambios en el
+          apetito o el peso, dificultades para dormir o dormir en exceso,
+          sentimientos de culpa o inutilidad, pensamientos de autolesión o
+          suicidio, entre otros síntomas.
+        </p>
+        <br></br>
+        <h2 class="white">
+          <u>¿Cuáles pueden ser sus causas?</u>
+        </h2>
+        <p class="whitep">
+          La depresión puede ser causada por una combinación de factores
+          genéticos, biológicos, ambientales y psicológicos. Puede afectar a
+          personas de todas las edades, razas y géneros. Es importante destacar
+          que la depresión no es simplemente sentirse triste o deprimido
+          temporalmente; es una afección médica seria que requiere tratamiento
+          adecuado
+        </p>
+        <br></br>
+        <h2 class="white">
+          <u>¿Qué tratamiento tiene?</u>
+        </h2>
+        <p class="whitep">
+          El tratamiento puede incluir terapia psicológica, medicación o una
+          combinación de ambos, dependiendo de la gravedad y las necesidades
+          individuales del paciente.
+        </p>
+        <br></br>
+        <h2 class="white">
+          <u>¿Cómo acompañar a alguien que tiene esta enfermedad?</u>
+        </h2>
+        <p class="whitep">
+          Acompañar a alguien que está lidiando con la depresión puede ser un
+          desafío, pero también puede marcar una gran diferencia en su
+          recuperación. Aquí hay algunas formas de apoyar a alguien con
+          depresión:
+        </p>
+        <li class="whitep">
+          Escucha sin juzgar: A veces, lo más útil que puedes hacer es
+          simplemente escuchar a la persona afectada. Permítele expresar sus
+          sentimientos y preocupaciones sin interrumpir ni juzgar.
+        </li>
+        <li class="whitep">
+          Ofrece tu apoyo incondicional: Hazle saber que estás ahí para él o
+          ella, sin importar qué. La sensación de estar solo puede empeorar la
+          depresión, así que asegúrate de demostrarle que no estás solo.
+        </li>
+        <li class="whitep">
+          Anima la búsqueda de ayuda profesional: La terapia psicológica y, en
+          algunos casos, la medicación pueden ser componentes importantes en el
+          tratamiento de la depresión. Anima a la persona afectada a buscar
+          ayuda de un profesional de la salud mental.
+        </li>
+        <li class="whitep">
+          Sé paciente y comprensivo: La recuperación de la depresión puede
+          llevar tiempo y puede haber altibajos en el proceso. Sé paciente y
+          comprensivo con la persona afectada, y recuerda que la recuperación
+          puede ser un camino largo y difícil.
+        </li>
+        <li class="whitep">
+          Ayuda con las tareas prácticas: La depresión puede hacer que incluso
+          las tareas diarias parezcan abrumadoras. Ofrece tu ayuda con tareas
+          prácticas como ir de compras, cocinar o limpiar.
+        </li>
+        <li class="whitep">
+          Fomenta el autocuidado: Anima a la persona afectada a cuidar de sí
+          misma, incluso si solo se trata de pequeños pasos como dormir lo
+          suficiente, comer saludablemente y hacer ejercicio.
+        </li>
+        <li class="whitep">
+          Evita juzgar o minimizar sus sentimientos: Es importante evitar frases
+          como "no deberías sentirte así" o "otras personas tienen problemas
+          peores". En su lugar, valida sus sentimientos y demuestra empatía.
+        </li>
+        <li class="whitep">
+          Cuida de ti mismo también: Acompañar a alguien con depresión puede ser
+          emocionalmente agotador. Asegúrate de cuidar de ti mismo y buscar
+          apoyo si lo necesitas.
+        </li>
+        <p class="whitep">
+          Recuerda que cada persona es única, por lo que es importante adaptar
+          tu apoyo a las necesidades específicas de la persona afectada.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Grupo1;
