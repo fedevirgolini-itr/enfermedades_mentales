68212bc37a36d738b78701b0bfa030333ecec889
ElPesi <elsanti.dominici@gmail.com>
2024-06-05 10:22:56 -0300

    Mejore el css

--- src/Grupo_4/Modularizacion.jsx
+++ src/Grupo_4/Modularizacion.jsx
@@ -3,7 +3,7 @@ import './Grupo4.css';
 
 function Modularizacion() {
   return (
-   <> <center><video src="videos/dama_llora_desorden.mp4" class="palermo" controls> </video></center>
+   <> <center><video class="video" src="videos/dama_llora_desorden.mp4"controls> </video></center>
     <br />
     <center><b>Dama de casa llora al ver el desorden bajo el sillon</b></center>
     <br></br>
