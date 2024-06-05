446b9a3dcac0f579d1f81b50c5f2a6613a73780f
Gorra.js <gorrahabla1234@gmail.com>
2024-06-05 10:27:56 -0300

    v2

---
+++ src/Grupo_1/QueEsDepresion.jsx
@@ -0,0 +1,16 @@
+export default function () {
+  return (
+    <div class="Definicion">
+      <center>
+        <h1 class="white">La depresión</h1>
+      </center>
+      <h2 class="white">
+        <u>¿Qué es la depresión?</u>
+      </h2>
+      <p class="whitep">
+        La depresión es un trastorno mental que afecta el estado de ánimo, los
+        pensamientos y el comportamiento de una persona.
+      </p>
+    </div>
+  );
+}
---
+++ src/Grupo_1/Tratamiento.jsx
@@ -0,0 +1,15 @@
+export default function () {
+  return (
+    <div class="Tratamientio">
+      <h2 class="white">
+        <u>¿Qué tratamiento tiene?</u>
+      </h2>
+      <p class="whitep">
+        El tratamiento puede incluir terapia psicológica, medicación o una
+        combinación de ambos, dependiendo de la gravedad y las necesidades
+        individuales del paciente.
+      </p>
+      <br></br>
+    </div>
+  );
+}
