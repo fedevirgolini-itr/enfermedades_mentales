0d36f0edee7482ca719e7b1e23b954783078fade
luca <lucawlodarczyk772@gmail.com>
2024-06-05 11:25:47 -0300

    agregado del meme

--- src/Grupo_4/Grupo4.jsx
+++ src/Grupo_4/Grupo4.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './Grupo4.css';
 import Teorico from './teorico';
 import Modularizacion from './Modularizacion';
-
+import Pola from './agregados';
 function Grupo4() {
   return (
       <main >
         <Teorico/>
         <Modularizacion/>
+        <Pola/>
       </main>
   );
 }
